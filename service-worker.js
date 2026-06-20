9ab76c226bfa06d737998dd970bdd8f6a33391bf
aotfnat <aotfnat@yahoo.com>
2026-06-19 21:41:45 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = 'fitness-app-v9.2.4';
+const CACHE_NAME = 'fitness-app-v9.2.5';
 
 const BASE = self.location.pathname.replace(/\/service-worker\.js$/, '');
 const urlsToCache = [
